b7d9c8923ab4053499ac372df989a4842e2e4b4a
sandesh2027 <sandesh.1rn14ec126@gmail.com>
2018-08-29 12:37:34 +0530

    commit this

---
+++ eclipse-workspace1/experiment/src/com/experiment/expClass.java
@@ -0,0 +1,8 @@
+package com.experiment;
+
+public class expClass {
+	public static void main(String[] args) {
+		System.out.println("experimenting with egit");
+	}
+
+}
---
+++ eclipse-workspace1/experiment/src/com/experiment/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Sandesh
+ *
+ */
+package com.experiment;
